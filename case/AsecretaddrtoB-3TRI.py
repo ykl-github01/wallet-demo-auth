a6ce825516d67600006b60ce52c5de5aae13cc64
ykl-github01 <171765598@qq.com>
2019-09-06 12:17:40 +0800

    second

--- case/AsecretaddrtoB-3TRI.py
+++ case/AsecretaddrtoB-3TRI.py
@@ -1,11 +1,10 @@
-from framework.browser_engine import Browser
 from framework.basepage import BasePage
 from case.login import Login
 import configparser,os
 import time
 
 '''
-A明文转B 2TRI
+A隐私地址转B 3TRI
 '''
 driver=Login().login()
 bs=BasePage(driver)
@@ -14,13 +13,16 @@ dir = os.path.abspath('.').split('case')[0]
 config.read(dir + "/config/config.ini", encoding='UTF-8')
 w2 = config.get("theWallets", "wallet2")
 try:
-    driver.find_element_by_xpath("/html/body/div[2]/div[1]/div[4]/div[1]/input").send_keys(w2)
+    driver.find_element_by_xpath("//*[@id='utxoPrivacyDestAddressId']").send_keys(w2)
     time.sleep(1)
-    driver.find_element_by_id('utxoNormalAmountId').send_keys(200)
+    driver.find_element_by_id('utxoPrivacyAmountId').send_keys(300)
     time.sleep(1)
-    driver.find_element_by_id('normalTransferButtonId').click()
+    driver.find_element_by_id('privacyTransferButtonId').click()
     time.sleep(2)
     driver.switch_to_alert().accept()
     driver.find_element_by_xpath('//*[@id="refreshCurrentBalanceButton"]').click()
+    time.sleep(5)
 except Exception as e:
     print(e)
+
+driver.quit()
